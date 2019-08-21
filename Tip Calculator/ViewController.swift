d8b0f980aaf339429012837d1ae904d7ea711c2d
Bryan Lopez <bryanlopezrr@gmail.com>
2019-08-21 12:21:26 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Bryan Lopez on 8/21/19.
+//  Copyright © 2019 Bryan Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
